43a4bf3260d1cd1f651756ca1fc92a65dfbd4fcb
jjin0ee <jjin0ee@littlefox.com>
2023-03-30 11:33:42 +0900

    로컬에서 노드와 리액드 구동 시키기

--- client/src/components/MyComponent.tsx
+++ client/src/components/MyComponent.tsx
@@ -4,7 +4,7 @@ function MyComponent() {
   const [data, setData] = useState('');
 
   useEffect(() => {
-    fetch('http://localhost:3000/')
+    fetch('http://localhost:5000/')
       .then(response => response.text())
       .then(data => setData(data))
       .catch(error => console.error(error));
