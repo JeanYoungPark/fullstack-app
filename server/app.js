bb3a526e7dfec1cc342c3dd97aaeff196db7c473
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2023-03-30 22:52:06 +0900

    node에서 DB 연동하기

--- server/app.js
+++ server/app.js
@@ -1,43 +1,56 @@
-var createError = require('http-errors');
-var express = require('express');
-const cors = require('cors');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+const cors = require("cors");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+const mysql = require("mysql2");
+require("dotenv").config();
+
+const connectionPool = mysql.createPool({
+  connectionLimit: 10,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
 
 var app = express();
 app.use(cors());
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// 커넥션 풀을 전역으로 사용할 수 있도록 설정
+app.locals.pool = connectionPool;
 
-app.use(logger('dev'));
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,31 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// 회원정보 가져오기
+router.get("/", function (req, res, next) {
+  const pool = req.app.locals.pool; // app.locals에 저장된 커넥션 풀 가져오기
+  pool.getConnection(function (err, connection) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    connection.query("SELECT * FROM users", function (error, results, fields) {
+      connection.release(); // 커넥션 반환
+      if (error) {
+        console.error(error);
+        return;
+      }
+
+      res.json(results);
+    });
+  });
 });
+
+// 회원가입
+
+// 로그인
+
+// 로그아웃
 
 module.exports = router;
