e046ac196bd89193e5813471d7c45a0f4bda7553
jjin0ee <jjin0ee@littlefox.com>
2023-04-03 09:02:12 +0900

    불필요한 소스 삭제 및 커넥션 정리

--- server/app.js
+++ server/app.js
@@ -1,26 +1,24 @@
-var createError = require("http-errors");
-var express = require("express");
+let createError = require("http-errors");
+let express = require("express");
 const cors = require("cors");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-
+let path = require("path");
+let cookieParser = require("cookie-parser");
+let logger = require("morgan"); // 로깅에 도움을 주는 미들웨어
 const pool = require("./db");
 
-var app = express();
-app.use(cors());
+let indexRouter = require("./routes/index");
+let usersRouter = require("./routes/users");
 
-// 커넥션 풀을 전역으로 사용할 수 있도록 설정
-// app.locals.pool = connectionPool;
+let app = express();
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+// view 관련 사용 안할 것임으로 주석
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "jade");
 
-app.use(async (req, res, next) => {
+app.use(logger("dev"));
+
+app.use(cors());  // 다른 도메인에서 리소스 요청 허용 (HTTP 요청 방식)
+app.use(async (req, res, next) => { // 커넥션 연결
   try {
     const connection = await pool.getConnection();
     req.connection = connection;
@@ -30,29 +28,33 @@ app.use(async (req, res, next) => {
   }
 });
 
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// 요청으로 들어오는 body를 JSON 객체로 파싱해주는 역할
+app.use(express.json()); 
+/* post 요청으로 전달된 데이터를 해석한다. content-type이 application/x-www-form-urlencoded 경우에만 동작
+ * extended 매개변수가 true인 경우 querystring 모듈이 좀 더 유연하게 동작하도록 한다. false인 경우 배역과 객체를 처리하지 못한다.
+ */
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+// 정적 파일들을 root 경로로 접근 할 수 있도록 설정
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+// 존재하지 않는 페이지 접근시 404 에러
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-// error handler
+// 미들웨어 체인에서 이전 미들웨어에서 에러가 발생하는 경우 실행
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  // 뷰에서 사용하는 로컬 변수
+  // res.locals.message = err.message;
+  // res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
   res.render("error");
 });
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 module.exports = app;
--- server/routes/users.js
+++ server/routes/users.js
@@ -4,70 +4,18 @@ var router = express.Router();
 // 회원정보 가져오기
 router.get("/", async (req, res, next) => {
   try {
-    const [rows] = await req.connection.excute("SELECT * FROM users");
-    res.json(rows);
+    const [rows] = await req.connection.query("SELECT * FROM users");
+    res.send(rows);
   } catch (err) {
     next(err);
   } finally {
     req.connection.release();
   }
-  // const pool = req.app.locals.pool; // app.locals에 저장된 커넥션 풀 가져오기
-  // pool.getConnection(function (err, connection) {
-  //   if (err) {
-  //     console.error(err);
-  //     return;
-  //   }
-
-  //   connection.query("SELECT * FROM users", function (error, results, fields) {
-  //     connection.release(); // 커넥션 반환
-  //     if (error) {
-  //       console.error(error);
-  //       return;
-  //     }
-
-  //     res.json(results);
-  //   });
-  // });
 });
-// router.get("/", function (req, res, next) {
-//   const pool = req.app.locals.pool; // app.locals에 저장된 커넥션 풀 가져오기
-//   pool.getConnection(function (err, connection) {
-//     if (err) {
-//       console.error(err);
-//       return;
-//     }
-
-//     connection.query("SELECT * FROM users", function (error, results, fields) {
-//       connection.release(); // 커넥션 반환
-//       if (error) {
-//         console.error(error);
-//         return;
-//       }
-
-//       res.json(results);
-//     });
-//   });
-// });
 
 // 회원가입
 // router.post("/", function (req, res, next) {
-//   const pool = req.app.locals.pool; // app.locals에 저장된 커넥션 풀 가져오기
-//   pool.getConnection(function (err, connection) {
-//     if (err) {
-//       console.error(err);
-//       return;
-//     }
-
-//     connection.query("SELECT * FROM users", function (error, results, fields) {
-//       connection.release(); // 커넥션 반환
-//       if (error) {
-//         console.error(error);
-//         return;
-//       }
-
-//       res.json(results);
-//     });
-//   });
+  
 // });
 
 // 로그인
