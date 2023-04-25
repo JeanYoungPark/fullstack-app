a864b42c740a8f0dbbb447bb410660a0657ca0da
jjin0ee <jjin0ee@littlefox.com>
2023-04-25 16:28:34 +0900

    라우트 미들웨어 수정 필요

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,26 +4,19 @@ import Join from "./pages/user/Join";
 import Login from "./pages/user/Login";
 import Auth from "./pages/user/Auth";
 import { CookiesProvider } from "react-cookie";
-import authMiddleware from "./middleware/auth.middleware";
-import { AuthAction } from "../src/actions/authActionType";
 
 function App() {
     return (
         <CookiesProvider>
             <Router>
                 <Routes>
-                    <Route path="/login" element={withMiddleware(<Login />, [authMiddleware])}/>
-                    <Route path="/join" element={withMiddleware(<Join />, [authMiddleware])}/>
-                    <Route path="/auth" element={withMiddleware(<Auth />, [authMiddleware])} />
+                    <Route path="/login" element={<Login />}/>
+                    <Route path="/join" element={<Join />}/>
+                    <Route path="/auth" element={<Auth />} />
                 </Routes>
             </Router>
         </CookiesProvider>
     );
-}
-
-// Route element에 미들웨어를 추가하는 함수
-function withMiddleware(element: JSX.Element, middlewares: ((element: JSX.Element) => JSX.Element)[]): JSX.Element {
-  return middlewares.reduce((acc, middleware) => middleware(acc), element);
 }
 
 export default App;
