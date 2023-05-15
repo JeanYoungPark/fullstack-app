3923eb7a8b427d8fb380d54c1e54297457b6cd91
jjin0ee <jjin0ee@littlefox.com>
2023-05-15 16:05:26 +0900

    board 제작중

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,17 +6,24 @@ import Login from "./pages/user/Login";
 import Auth from "./pages/user/Auth";
 import Home from "./pages/Home";
 import BoardList from "./pages/board/List";
+import BoardView from "./pages/board/View";
 import { CookiesProvider } from "react-cookie";
+import TabComponent from "./components/TabComponent";
 
 function App() {
     return (
         <CookiesProvider>
             <BrowserRouter>
+                <TabComponent />
                 <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/join" element={<Join />} />
-                    <Route path="/auth" element={<Auth />} />
+                    <Route path="/" element={<Home />}>
+                        <Route path="login" element={<Login />} />
+                        <Route path="join" element={<Join />} />
+                        <Route path="auth" element={<Auth />} />
+                        <Route path="post" element={<BoardList/>}>
+                            <Route path=":postId" element={<BoardView/>}/>
+                        </Route>
+                    </Route>
                 </Routes>
             </BrowserRouter>
         </CookiesProvider>
--- client/src/pages/board/List.tsx
+++ client/src/pages/board/List.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import BoardListComponent from "../../components/BoardListComponent";
 
 function List() {
