5e01208a9d1c5195dc103abbfc24bd32d566d160
jjin0ee <jjin0ee@littlefox.com>
2023-05-30 15:43:34 +0900

    방향키로 움직이기

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,47 @@
-import React from "react";
+import { useEffect, useRef } from "react";
+import { Wrap, Ball, BallBubble, BallBubbleTail, BallClose } from "../styles/game";
 
 function Home() {
-    return <div>Home</div>;
+    const ballRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const handleKeyPress = (e: KeyboardEvent) => {
+            const ball = ballRef.current;
+            let speed = 10;
+
+            if(e.ctrlKey){
+                speed += 20;
+            }
+
+            if(ball){
+                if(e.key === "ArrowUp"){
+                    ball.style.top = `${ball.offsetTop - speed}px`;
+                }else if(e.key === "ArrowDown"){
+                    ball.style.top = `${ball.offsetTop + speed}px`;
+                }else if(e.key === "ArrowLeft"){
+                    ball.style.left = `${ball.offsetLeft - speed}px`;
+                }else if(e.key === "ArrowRight"){
+                    ball.style.left = `${ball.offsetLeft + speed}px`;
+                }
+            }
+
+        }
+
+        window.addEventListener("keydown", handleKeyPress);
+        return () => window.removeEventListener("keydown", handleKeyPress);
+    }, []);
+
+    return (
+        <Wrap>
+            <Ball ref={ballRef}>
+                <BallBubble>
+                    <BallClose><strong>x</strong></BallClose>
+                    <BallBubbleTail />
+                    <strong>booster</strong> with ctrl!
+                </BallBubble>
+            </Ball>
+        </Wrap>
+    );
 }
 
 export default Home;
---
+++ client/src/styles/game.tsx
@@ -0,0 +1,87 @@
+import styled, { keyframes } from 'styled-components';
+
+export const Wrap = styled.div`
+    position: relative;
+`;
+
+export const Ball = styled.div`
+    position:absolute;
+    top: 20px;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+    background-color: #ccc;
+`;
+
+const ballBubbleScale = keyframes`
+    0% {
+        transform: scale(1);
+    }
+
+    50% {
+        transform: scale(1.1);
+    }
+
+    100% {
+        transform: scale(1);
+    }
+`;
+
+export const BallBubbleTail = styled.div`
+    position: absolute;
+    left: 6px;
+    bottom: 3px;
+    width: 11px;
+    height: 8px;
+    background-color: #fff;
+    border-left: 1px solid #ccc;
+    border-bottom: 1px solid #ccc;
+    transform: rotate(355deg);
+`;
+
+export const BallClose = styled.div`
+    display: none;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    background-color: #3e85e4;
+    color: #fff;
+    text-align: center;
+    line-height: 3vh;
+
+    strong {
+        color: #fff;
+    }
+`;
+
+export const BallBubble = styled.div`
+    position: absolute;
+    top: -40px;
+    left: 40px;
+    width: 70px;
+    height: auto;
+    padding: 10px;
+    text-align: center;
+    border-radius: 50%;
+    border: 1px solid #ccc;
+    animation: ${ballBubbleScale} 2s linear infinite;
+    cursor: pointer;
+
+    &:hover {
+        border-color: #3e85e4;
+
+        ${BallClose} {
+            display: block;
+        }
+
+        ${BallBubbleTail} {
+            background-color: #3e85e4;
+            border-color: #3e85e4;
+        }
+    }
+`;
