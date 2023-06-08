a73013664944345896b5f20f9d2b72703c152739
jeanyoungpark <jean.young.park.96@gmail.com>
2023-06-08 22:19:51 +0900

    저장로직 구현중

--- server/routes/board.js
+++ server/routes/board.js
@@ -1,16 +1,36 @@
 const express = require('express');
 const router = express.Router();
 
-router.get("/list", (req, res, next) => {
-
+router.get("/list", async (req, res, next) => {
+    try {
+        const [rows] = await req.connection.execute("SELECT * FROM board");
+        res.send(rows);
+    } catch (err) {
+        next(err);
+    } finally {
+        req.connection.release();
+    }
 });
 
 router.get("/:id", (req, res, next) => {
 
 });
 
-router.post("/save", (req, res, next) => {
+router.post("/save", async (req, res, next) => {
+    try {
+        const { title, content, writer_id } = req.body;
+        const [result] = await req.connection.execute(
+            "INSERT INTO board (title, content, writer_id) VALUES (?, ?, ?)",
+            [title, content, writer_id]
+        );
 
+        const insertId = result.insertId;
+        res.send({ insertId: insertId });
+    } catch (err) {
+        next(err);
+    } finally {
+        req.connection.release();
+    }
 });
 
 router.post("/delete", (req, res, next) => {
